434e2ce5bfda4267301478c5b863fc83fe51c1f9
MH <myur.hajare@gmail.com>
2022-01-12 23:06:13 +0530

    Selection Sort Algorithm..

--- src/Algorithm/Insertion_sort.java
+++ src/Algorithm/Insertion_sort.java
@@ -2,10 +2,31 @@ package Algorithm;
 
 import java.util.Scanner;
 
+class I_sort {
+    int[] a;
+
+    public void Input() {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        System.out.println("Enter Array Size");
+        n = sc.nextInt();
+        a = new int[n];
+
+    }
+
+    public void Find() {
+
+        for (int i = 1; i < a.length; i++) {
+
+        }
+
+    }
+
+}
+
 public class Insertion_sort {
     public void main(String[] args) {
-
-
 
 
     }
